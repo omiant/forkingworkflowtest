3230cc02adc36131b5dfbf1adfd8a284183d28b9
Anton Merkulau <Anton_Merkulau@epam.com>
2017-12-28 14:50:39 +0300

    Initialize project

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("Плюс");
+console.log(`1 + 2 = ${plus(1, 2)}`);
+console.log(`2 + 2 = ${plus(2, 2)}`);
+
+console.log("\nМинус");
+console.log(`1 - 2 = ${minus(1, 2)}`);
+console.log(`2 - 2 = ${minus(2, 2)}`);
+
+console.log("\nУмножить");
+console.log(`1 * 2 = ${multiply(1, 2)}`);
+console.log(`2 * 2 = ${multiply(2, 2)}`);
+
+console.log("\nДелить");
+console.log(`1 / 2 = ${divide(1, 2)}`);
+console.log(`2 / 2 = ${divide(2, 2)}`);
