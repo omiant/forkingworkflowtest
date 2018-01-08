7d30b2845fed26846c2d328d188358f1f1ea4f85
Anton Merkulau <Anton_Merkulau@epam.com>
2018-01-08 17:05:46 +0300

    squash 1

--- minus.js
+++ minus.js
@@ -1,3 +1,6 @@
 function minus (a, b) {
-    return a - b;
+    let i1 = +a;
+    let i2 = +b;
+    if (isNaN(i1) || isNaN(i2)) throw Error("Invalid input. Input must contain only numbers!");
+    return i1 - i2;
 }
