90b3d0177cd1bba12514ccffe936627f3d197dbb
Anton Merkulau <Anton_Merkulau@epam.com>
2018-01-08 17:05:46 +0300

    squash 1
    # Conflicts:
    #	minus.js
    #	multiply.js

--- minus.js
+++ minus.js
@@ -1,6 +1,6 @@
 function minus (a, b) {
     let i1 = +a;
     let i2 = +b;
-    if (isNaN(i1) || isNaN(i2)) throw Error("Invalid input. Input must contain only numbers!!!!11!1!11");
+    if (isNaN(i1) || isNaN(i2)) throw Error("Invalid input. Input must contain only numbers!");
     return i1 - i2;
 }
