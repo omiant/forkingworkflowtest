b25ff9d17b4475f38947c126f7ed59c094a74a59
Anton Merkulau <Anton_Merkulau@epam.com>
2018-01-08 17:01:17 +0300

    Multiply refactoring

--- multiply.js
+++ multiply.js
@@ -1,3 +1,6 @@
-function multiply (a, b) {
-    return a * b;
+function minus (a, b) {
+    let i1 = +a;
+    let i2 = +b;
+    if (isNaN(i1) || isNaN(i2)) throw Error("Invalid input. Input must contain only numbers!");
+    return i1 * i2;
 }
